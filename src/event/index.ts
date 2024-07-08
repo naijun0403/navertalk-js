dbb37feec600f2ad05de681b430f581d5e1a3e34
naijun0403 <naijun0403@gmail.com>
2024-07-09 08:04:36 +0900

    chore: copyright comment

--- src/event/index.ts
+++ src/event/index.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 naijun0403
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * basic event interface
  * @reference https://github.com/navertalk/chatbot-api?tab=readme-ov-file#%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EA%B8%B0%EB%B3%B8-%EA%B5%AC%EC%A1%B0
@@ -7,3 +23,4 @@ export interface Event<T = Record<string, unknown>> {
     options: T;
     user: string;
 }
+
